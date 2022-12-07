bfcd1c9fe91caf3955fe1cd2e86c9dd719b49c43
unknown <royaam@code.edu.az>
2022-12-08 00:24:31 +0400

    task2

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,17 +1,31 @@
 ﻿
 
-int n = 10;
-if (n % 3 == 0 && n % 7 == 0) ;
+//int n = 10;
+//if (n % 3 == 0 && n % 7 == 0) ;
+//{
+//    Console.WriteLine("bolunur");
+//}
+
+// else
+//{
+//    Console.WriteLine("bolunmur")
+//}
+
+
+
+int a = 10;
+int b = 20;
+int sum = 0;
+if (a % 2 == 0 && b % 2 == 0) ;
 {
-    Console.WriteLine("bolunur");
-}
+    sum = a + b;
+    Console.WriteLine(sum);
 
- else
-{
-    Console.WriteLine("bolunmur")
-}
-
-
+    else
+    {
+        Console.WriteLine("duzgun eded daxil edin");
+    }
+        }
 
 
 
