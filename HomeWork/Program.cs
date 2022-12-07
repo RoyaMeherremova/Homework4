a920d8fc560fa340b9fd6bab5ca5629519f0f5b7
unknown <royaam@code.edu.az>
2022-12-08 00:37:33 +0400

    task3

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -13,20 +13,34 @@
 
 
 
-int a = 10;
-int b = 20;
+//int a = 10;
+//int b = 20;
+//int sum = 0;
+//if (a % 2 == 0 && b % 2 == 0) ;
+//{
+//    sum = a + b;
+//    Console.WriteLine(sum);
+
+//    else
+//    {
+//        Console.WriteLine("duzgun eded daxil edin");
+//    }
+//        }
+
+
+int n = 10;
+int m = 20;
 int sum = 0;
-if (a % 2 == 0 && b % 2 == 0) ;
+for (i = 10; i < 20; i++) ;
+if (i % 2 == 1) ;
+sum+=i;
+
 {
-    sum = a + b;
-    Console.WriteLine(sum);
+    Console.Writeline(sum);
 
-    else
-    {
-        Console.WriteLine("duzgun eded daxil edin");
-    }
-        }
-
-
+  
+}
+else
+}
 
 
