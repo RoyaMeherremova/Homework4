08679b5148c2d7d0a57db272c74f4f49d502acb8
unknown <royaam@code.edu.az>
2022-12-08 00:45:03 +0400

    task4

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -28,19 +28,26 @@
 //        }
 
 
-int n = 10;
-int m = 20;
+//int n = 10;
+//int m = 20;
+//int sum = 0;
+//for (i = 10; i < 20; i++) ;
+//if (i % 2 == 1) ;
+//sum+=i;
+
+//{
+//    Console.Writeline(sum);
+
+
+//}
+//else
+//}
+int[] nums= { 1, 5, 3, 4, 8, 9 };
 int sum = 0;
-for (i = 10; i < 20; i++) ;
-if (i % 2 == 1) ;
-sum+=i;
-
+for (i = 0; i < nums.lenght; i++) ;
+if (i% 2 == 1) ;
+sum += i;
 {
-    Console.Writeline(sum);
-
-  
+    Console.Writeline(sum)
 }
-else
-}
-
 
